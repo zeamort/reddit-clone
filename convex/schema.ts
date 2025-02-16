b99b4e79e07ea38aec43e5b90bb40fc11a57f79e
Morteza Rahimi <zeamort@gmail.com>
2025-02-15 19:20:01 -0800

    setup clerk-convex user authentication

---
+++ convex/schema.ts
@@ -0,0 +1,11 @@
+import {defineSchema, defineTable} from "convex/server";
+import {v} from "convex/values";
+
+export default defineSchema({
+    users: defineTable({
+        username: v.string(),
+        externalId: v.string()
+    })
+        .index("byExternalId", ["externalId"])
+        .index("byUsername", ["username"])
+});
