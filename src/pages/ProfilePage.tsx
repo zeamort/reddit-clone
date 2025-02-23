742c8bdfa924120f2c41a7fecdd280196ad88730
Morteza Rahimi <zeamort@gmail.com>
2025-02-23 10:42:13 -0800

    implement view posts in profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,7 +1,45 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import PostCard from "../components/PostCard";
 import "../styles/ProfilePage.css";
 
 const ProfilePage = () => {
-    return <div>ProfilePage</div>
+    const {username} = useParams();
+    const posts = useQuery(api.post.userPosts, {authorUsername: username || ""});
+
+    if (posts === undefined) {
+        return (
+            <div className="content-container">
+                <div className="profile-header">
+                    <h1>u/{username}</h1>
+                </div>
+                <div className="loading">
+                    Loading posts...
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className="content-container">
+            <div className="profile-header">
+                <h1>u/username</h1>
+                <p style={{ color: "#7c7c7c" }}>Posts: 0</p>
+            </div>
+            <div className="posts-container">
+                {posts.length === 0 ? (
+                    <div className="no-posts">
+                        <p>No posts yet</p>
+                    </div>
+                ) : (
+                    posts.map((post) => (
+                        <PostCard key={post._id} post={post} showSubreddit={true}/>
+                    ))
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default ProfilePage;
